53f8a7083947e71ea0a6eb33a029359d0056c26d
Sarthik Garg <sarthikgarg01@gmail.com>
2020-07-18 15:37:49 +0530

    Added a mouse animation and removed ScrollBar

--- js/script.js
+++ js/script.js
@@ -5,18 +5,25 @@ window.addEventListener("load", () => {
 	greetImg.style.opacity = "1";
 });
 
+const mouse = document.getElementById("mouse");
+
+const aboutSec = document.getElementById("about-sec");
 const aboutTxt = document.getElementById("about-txt");
 const aboutContent = document.getElementById("about-content");
+
+mouse.addEventListener("click", (event) => {
+	aboutSec.scrollIntoView();
+});
 
 const options = {
 	threshold: 1.0,
 };
 
 let observer = new IntersectionObserver((options) => {
-	console.log(options[0]);
 	if (options[0].isIntersecting) {
-		console.log("here");
 		aboutTxt.style.transform = "translateX(0px)";
+		aboutTxt.style.visibility = "visible";
+		aboutTxt.style.opacity = "1";
 		aboutContent.style.visibility = "visible";
 		aboutContent.style.opacity = "1";
 	}
