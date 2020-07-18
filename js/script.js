204d2eefac23d96b25bfdf8996a69602577ca8df
Sarthik Garg <sarthikgarg01@gmail.com>
2020-07-19 03:40:06 +0530

    completed Skills section

--- js/script.js
+++ js/script.js
@@ -15,18 +15,40 @@ mouse.addEventListener("click", (event) => {
 	aboutSec.scrollIntoView();
 });
 
-const options = {
-	threshold: 1.0,
-};
+const aboutObserver = new IntersectionObserver(
+	(entries, observer) => {
+		if (entries[0].isIntersecting) {
+			aboutTxt.style.transform = "translateX(0px)";
+			aboutTxt.style.visibility = "visible";
+			aboutTxt.style.opacity = "1";
+			aboutContent.style.visibility = "visible";
+			aboutContent.style.opacity = "1";
 
-let observer = new IntersectionObserver((options) => {
-	if (options[0].isIntersecting) {
-		aboutTxt.style.transform = "translateX(0px)";
-		aboutTxt.style.visibility = "visible";
-		aboutTxt.style.opacity = "1";
-		aboutContent.style.visibility = "visible";
-		aboutContent.style.opacity = "1";
+			observer.disconnect();
+		}
+	},
+	{
+		threshold: 1.0,
 	}
-}, options);
+);
 
-observer.observe(aboutTxt);
+aboutObserver.observe(aboutTxt);
+
+const skills = document.getElementsByClassName("skill-card");
+
+const skillObserver = new IntersectionObserver(
+	(entries, observer) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add("skill-card-visible");
+			}
+		});
+	},
+	{
+		threshold: 1.0,
+	}
+);
+
+for (const skill of skills) {
+	skillObserver.observe(skill);
+}
